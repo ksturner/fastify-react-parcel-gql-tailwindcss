522b64546f64cb14ffdd4babe2063c8f508e5916
Kevin Turner <kevin@ksturner.com>
2022-06-15 22:58:38 -0500

    added in a very simple example of graphql support via the /add route

--- back/app.ts
+++ back/app.ts
@@ -1,6 +1,29 @@
+import path from 'path';
 import fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify';
+import mercurius from 'mercurius';
+import fastifyCors from '@fastify/cors';
+import fastifyStatic from '@fastify/static';
 
 const server: FastifyInstance = fastify({});
+
+const schema = `
+    type Query {
+        add(x: Int, y: Int): Int
+    }
+`;
+const resolvers = {
+    Query: {
+        add: async (_: any, { x, y }: any) => x + y,
+    },
+};
+server.register(mercurius, { schema, resolvers }); // graphql plugin
+server.register(fastifyCors, {
+    origin: '*',
+});
+server.register(fastifyStatic, {
+    root: path.join(__dirname, 'public'),
+    // prefix: '/public/', // optional, default '/'
+});
 
 const opts: RouteShorthandOptions = {
     schema: {
@@ -17,6 +40,15 @@ const opts: RouteShorthandOptions = {
     },
 };
 
+server.get('/', {}, async (request, reply) => {
+    return reply.sendFile('index.html');
+});
+
+server.get('/add', async (req, reply) => {
+    const query = '{ add(x: 1, y: 2) }';
+    return reply.graphql(query);
+});
+
 server.get('/ping', opts, async (request, reply) => {
     return { pong: 'it worked!' };
 });
@@ -29,6 +61,7 @@ const start = async () => {
         const port = typeof address === 'string' ? address : address?.port;
         console.log(`listening on http://localhost:${port}`);
     } catch (err) {
+        console.error(err);
         server.log.error(err);
         process.exit(1);
     }
